4ed2dd8f85e1fa4fb79c0bab6ccee8a560890a3e
theluanz <luanzuffo.lz@gmail.com>
2020-10-06 22:04:30 -0300

    Arvore

---
+++ data-structures/BinaryTree.py
@@ -0,0 +1,58 @@
+from pessoa import Pessoa
+
+class Tree: 
+    #construtor
+    def __init__(self, Pessoa, pai=None, mae=None):
+        self.pesssoa = Pessoa
+        self.pai = pai
+        self.mae = mae
+
+#adiciona os pais a classe
+    def addPais(self, pai=None, mae=None):
+        self.pai = pai
+        self.mae = mae
+      
+    #mostra todas as gerações
+    def mostrarTodaGeracao(self):
+        if self.pai:
+            print(self.pai.pesssoa.nome)
+            self.pai.mostrarTodaGeracao()
+        if self.mae:
+            print(self.mae.pesssoa.nome)
+            self.mae.mostrarTodaGeracao()
+
+            
+
+'''
+#primeiro deve-se criar uma pessoa, usando o struct do Luan C é isso, veja a sequencia
+# dos atributos no código do Luan C
+pessoa = Pessoa("eu", "sexo",1, 1, 12, 32, 121, "ctsus", "sintomas" )
+pai = Pessoa("pai", "sexo",12, 12, 12, 32, 121, "ctsus", "sintomas" )
+mae = Pessoa("mae", "sexo",123, 123, 12, 32, 121, "ctsus", "sintomas" )
+nono = Pessoa("nono", "sexo",1234, 1234, 12, 32, 121, "ctsus", "sintomas" )
+nona = Pessoa("nona", "sexo",12345, 12345, 12, 32, 121, "ctsus", "sintomas" )
+pessoa5 = Pessoa("nonamae", "sexo",123456, 123456, 12, 32, 121, "ctsus", "sintomas" )
+
+# Após criar as pessoas, deve-se criar um nó, pode fazer da seguinte forma:
+pai= Tree(pai)
+mae= Tree(mae)
+nono= Tree(nono)
+nona= Tree(nona)
+pessoa5= Tree(pessoa5)
+
+# nesse caso, root é a RAIZ, o ponto inicial, o primeiro nó B)
+root = Tree(pessoa)
+
+# agora o método de adicionar pai e mae para a raiz
+root.addPais(pai, mae)
+#aqui esta adiconando o pai e mae do PAI
+root.pai.addPais(nono,nona)
+#aqui o pai da mae
+root.mae.addPais(pessoa5)
+## Aqui chama toda a geração do root, a variavel q chama, é a q vai ser usada nesse exemplo é o ROOT
+root.mostrarTodaGeracao()
+
+'''
+
+
+    
---
+++ data-structures/pessoa.py
@@ -0,0 +1,16 @@
+class Pessoa:
+    def __init__(self, nome, sexo,cpf, rg, dia, mes, ano, ctsus, sintomas ):
+        self.nome = nome
+        self.sexo = sexo
+        self.cpf = cpf
+        self.rg = rg
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+        self.ctsus = ctsus
+        self.sintomas = sintomas
+
+class Diagnostico:
+    def __init__(self, pessoa, febre):
+        self.pessoa = pessoa
+        self.febre = febre
